3140644b8fbbfcac501bc8190c538cbcf4068206
aev218 <aev218@lehigh.edu>
2015-08-30 22:47:17 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////
+//Andre Verduzco
+//hw01
+//Welcome Class Java Program
+//
+//first compile the program
+//     java WelcomeClass.java
+//run the program
+//      Java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+    
+//add MainMethod
+public static void main (String[] args) {
+    //print the statement with a box around it
+    System.out.println("  -------------");
+    System.out.println("  |  WELCOME  |");
+    System.out.println("  -------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-A--E--V--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("I'm a second year student from California.");
+    
+ //testtest   
+} 
+}
+
+
+
