c7c78a93554b4c319e0a77244057fb7d2201a84c
aev218 <aev218@lehigh.edu>
2015-09-04 15:57:12 +0000

    updated Cyclometer

---
+++ lab02/lab02-Cyclometer.java
@@ -0,0 +1,45 @@
+//Andre Verduzco
+//September 4, 2015
+//CSE2
+//lab02-Cyclometer
+//Cyclometer Java Program
+//
+//first compile the program
+//
+//java Cyclometer.java
+//
+public class Cyclometer {
+    //main method required for every Java program
+    public static void main(String[]args){
+        int secsTrip1=480; //time in seconds it took for Trip1
+        int secsTrip2=3220; //time in seconds it took for Trip2
+        int countsTrip1=1561; //number of rotations for Trip1
+        int countsTrip2=9037; //number of rotations for Trip2
+        //our intermediate variables and output data
+        double wheelDiamter=27.0, //diameter of wheel
+        PI=3.14159, //
+        feetPerMile=5280, //amount of feet equivalent to 1 mile
+        inchesPerFoot=12, //amount of inches equivalent to 1 foot
+        secondsPerMinute=60; //seconds in a minute
+        double distanceTrip1, distanceTrip2, totalDistance;//
+        System.out.println("Trip 1 took "+
+        (secsTrip1/secondsPerMinute)+" minutes and had "+
+        countsTrip1+" counts. ");
+        //run calculations; store values
+        //Measuring time for distance traveled
+        //
+        //
+        distanceTrip1=countsTrip1*wheelDiamter*PI;
+        //the calculation above gives the distance in inches
+        //(for each count, a rotation of the wheel travels
+        //the diamter in inches times PI)
+        distanceTrip1/=inchesPerFoot*feetPerMile; //Gives  distance in miles
+        distanceTrip2=countsTrip2*wheelDiamter*PI/inchesPerFoot/feetPerMile;
+        totalDistance=distanceTrip1+distanceTrip2;
+        //Print out the output data
+        System.out.println("Trip 1 was "+distanceTrip1+" miles");
+        System.out.println("Trip 2 was "+distanceTrip2+" miles");
+        System.out.println("The total distance was"+totalDistance+" miles");
+        
+    }// end of main method
+}//end of class
